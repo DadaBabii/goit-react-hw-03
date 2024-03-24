e8ee3b1986a5a01e89d6e528f031c7fbbc1c71a9
DadaBabii <107479813+DadaBabii@users.noreply.github.com>
2024-03-24 16:20:09 +0200

    create

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,10 @@
+const Contact = () => {
+  return (
+    <li>
+      <p> name: Sveta</p>
+      <p> tel: 343-55-55</p>
+    </li>
+  );
+};
+
+export default Contact;
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+import Contact from "../Contact/Contact.jsx";
+
+const ContactList = () => {
+  return (
+    <ul>
+      <Contact />
+    </ul>
+  );
+};
+
+export default ContactList;
